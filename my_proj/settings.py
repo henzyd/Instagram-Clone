1d1795e9c45039dceb23371e6232483ee1a5cb2a
Uchechukwu Anachuna <anachunauchechukwu@gmail.com>
2022-11-01 15:13:34 +0100

    host

--- my_proj/settings.py
+++ my_proj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://henzyd-instagram-clone.herokuapp.com/']
+ALLOWED_HOSTS = ['henzyd-instagram-clone.herokuapp.com']
 
 
 # Application definition
